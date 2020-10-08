361e3adc6017fb0913a0c6e4a719efe797ea96de
ruochenPray <1441865605@qq.com>
2020-10-08 19:24:49 +0800

    calculator v1.0

---
+++ Calculator(v1.0)/Calculator/Calculation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    class Calculation
+    {
+        private string _operator;
+        private double _number1, _number2;
+
+        
+        public double Number1
+        {
+            get { return _number1; }
+            set { _number1 = value; }
+        }
+        public double Number2
+        {
+            get { return _number2; }
+            set { _number2 = value; }
+        }
+          
+        public string Operator
+        {
+            get { return _operator; }
+            set { _operator = value; }
+        }
+          
+        public string f(string str) {
+            switch (str) {
+                case "+": return Convert.ToString(_number1 + _number2);
+                case "-": return Convert.ToString(_number1 - _number2);
+                case "*": return Convert.ToString(_number1 * _number2);
+                case "/": // if (_number2 == 0) throw new Exception("除数不能为0");
+                          // else return Convert.ToString(_number1 / Number2); 
+                          try { return Convert.ToString(_number1 / _number2); }
+                          catch (Exception e) { Console.WriteLine("除数不能为0"); }
+                          break;
+                default:
+                    break;
+            }
+            return "请检查您的输入";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (_number1 == _number2) return true;
+            else return false;
+        }
+    }
+}
---
+++ Calculator(v1.0)/Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Calculation> list = new List<Calculation>();
+            Calculation c = new Calculation();
+            c.Operator = "+";
+            c.Number1 = 1;
+            c.Number2 = 2;
+            list.Add(c);
+
+            Calculation c2 = new Calculation();
+            c2.Operator = "/";
+            c2.Number1 = 1;
+            c2.Number2 = 0;
+            list.Add(c2);
+            foreach (Calculation i in list) {
+                Console.WriteLine("{0} 与 {1}{2}", i.Number1, i.Number2, i.Number1.Equals(i.Number2) == true ? "相等" : "不相等");
+                string result = i.f(i.Operator);
+                Console.Write("{0} {1} {2} = ", i.Number1, i.Operator, i.Number2);
+                Console.WriteLine(result);
+            }     
+
+        }
+    }
+}
